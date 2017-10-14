d631d89f4788d4a5d0c08bf7055db58d141db2d5
Andy608 <AndrewR608@gmail.com>
2017-10-14 18:47:12 -0400

    Fixed merge conflict in PA5 file.

--- PA5.cpp
+++ PA5.cpp
@@ -3,7 +3,6 @@
 #include"linkedList.h"
 int main()
 {
-<<<<<<< HEAD
 	LinkedList<long> list;
 	for (int i = 1000; i >= 0; i--)
 	{
@@ -24,8 +23,5 @@ int main()
 		list.removeAt(i);
 	}
 	list.print();
-=======
-	std::cout << "Hello, World!" << std::endl;
->>>>>>> 1123c8642531bd662e31aa9a0924c2b2939ab520
 	return 0;
 }*/
